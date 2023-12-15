54050a98616dc2a67304c59a0a4f43048e40d190
Matt Wimer (Home) <mattwimer1@gmail.com>
2023-12-15 00:35:29 -0800

    Added getNFittest. I think I skipped a step? Might have to redo some stuff

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -13,4 +13,24 @@ public class GeneticAlgorithm {
 
     // Methods for initialization, selection, crossover, mutation, and creating new generation
     // These are more complex and depend on your chosen approach for each step
+
+    public Population evolvePopulation(Population population){
+        // REPLACEMENT
+        // get n = elitismCount of the fittest from population, then generate popSize - n new individuals
+        Individual[] fittestFromLastGen = population.getNFittest(elitismCount);
+        for(int i = 0 ; i < populationSize ; i++)
+            population.getIndividuals()[i] = i < elitismCount ? fittestFromLastGen[i] : new Individual(population.getGeneLength());
+
+        // SELECTION
+        
+
+        // CROSSOVER
+
+
+        // MUTATION
+
+
+        
+        return population;
+    }
 }
--- Population.java
+++ Population.java
@@ -1,7 +1,13 @@
 public class Population {
     private Individual[] individuals;
+    private int geneLength;
+
+    public int getGeneLength() {
+        return geneLength;
+    }
 
     public Population(int populationSize, int geneLength) {
+        this.geneLength = geneLength;
         individuals = new Individual[populationSize];
         for (int i = 0; i < populationSize; i++) {
             individuals[i] = new Individual(geneLength);
@@ -17,6 +23,29 @@ public class Population {
         }
         return fittest;
     }
+
+    /**
+     * @param n number of the best individuals to return
+     * @return array of size n containing the n most fit individuals in this class's arr of individuals
+     */
+    public Individual[] getNFittest(int n){
+        if(n >= individuals.length)
+            return getIndividuals();
+        Individual[] fittest = new Individual[n];
+        for(int i = 0 ; i < n ; i++)
+            fittest[i] = individuals[i];
+        for(int i = n ; i < individuals.length ; i++)
+            for(int j = 0 ; j < n ; j++)
+                if(fittest[j].getFitness() < individuals[i].getFitness()){
+                    Individual temp = fittest[j]; // if a previously fittest individual is swapped out, it is then compared to the rest of the fittest to vie for a spot
+                    fittest[j] = individuals[i];
+                    individuals[i] = temp;
+                }
+
+        return fittest;
+    }
+
+    public void setIndividual()
 
     public Individual[] getIndividuals() {
         return individuals;
