a2b96d31a8e62d340bbddf5262bab4dae4e30c38
Matt Wimer (Home) <mattwimer1@gmail.com>
2023-12-15 01:57:26 -0800

    Added Replacement, and fixed a bug where fittest members may have been getting duplicated.

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -17,8 +17,6 @@ public class GeneticAlgorithm {
     public Population evolvePopulation(Population population){
         // get n = elitismCount of the fittest from population
         Individual[] fittestFromLastGen = population.getNFittest(elitismCount);
-        for(int i = 0 ; i < elitismCount ; i++)
-            population.getIndividuals()[i] = fittestFromLastGen[i];
         
         
         Individual[] newGeneration = new Individual[populationSize - elitismCount];
@@ -27,26 +25,32 @@ public class GeneticAlgorithm {
             // CROSSOVER
             Individual child;
             if(Math.random() <= crossoverRate)
-                // i wonder if its critical that the parents are not the same...
-                child = crossover(population.tournament(), population.tournament());
+            // i wonder if its critical that the parents are not the same...
+            child = crossover(population.tournament(), population.tournament());
             else
-                child = population.getIndividuals()[i]; // if didn't roll chance to crossover, use old generation member
+            child = population.getIndividuals()[i]; // if didn't roll chance to crossover, use old generation member
             
             newGeneration[i] = child;
             // MUTATION
             for(int j = 0 ; j < child.getGenes().length ; j++)
-                if(Math.random() <= mutationRate)
-                    child.setGene(j, child.generateGene());
+            if(Math.random() <= mutationRate)
+            child.setGene(j, child.generateGene());
         }
         
         
         // REPLACEMENT 
         // Once children have been bred, replace the previous population (minus the fittest) with newGeneration
+        // for(int i = 0 ; i < elitismCount ; i++)
+        //     population.getIndividuals()[i] = fittestFromLastGen[i];
+        for(int i = 0 ; i < populationSize ; i++)
+            population.getIndividuals()[i] = i < elitismCount ? fittestFromLastGen[i] : newGeneration[i];
         
         
         
-
-
+        
+        
+        
+        
         
         return population;
     }
